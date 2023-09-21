b788de00d0d1e6b935ee099c972409c1d6b4390b
Who1s1t <96753821+Who1s1t@users.noreply.github.com>
2023-09-21 17:31:10 +0300

    Add files via upload

---
+++ 11.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите минуты :");
+double m = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите a :");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите b :");
+double b = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите c :");
+double c = Convert.ToDouble(Console.ReadLine());
+double res;
+if (m <= a)
+{
+    res = b;
+
+}
+else
+{
+ 
+    res = (m - a) * c + b;
+}
+Console.WriteLine($"В это месяце вы заплатите: {res}");
